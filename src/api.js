757ea727bea7fafd3a9cacff1473c308fdb9ee12
serach boes <serachboes@gmail.com>
2022-08-17 17:51:06 -0400

    A3 Completed

--- src/api.js
+++ src/api.js
@@ -25,11 +25,12 @@ export async function getUserFragments(user) {
   }
 }
 
+//user can create a fragment
 export async function postUserFragments(user, fragment, fragmentType) {
   console.log('Posting user fragment data...');
   try {
     const res = await fetch(`${apiUrl}/v1/fragments`, {
-      method: "post",
+      method: "POST",
       // Generate headers with the proper Authorization bearer token to pass
       headers: user.authorizationHeaders(fragmentType),
       body: fragment,
@@ -44,6 +45,7 @@ export async function postUserFragments(user, fragment, fragmentType) {
   }
 }
 
+//user can get a fragment by id
 export async function getFragmentById(user, id) {
   console.log('Requesting user fragment data...');
   try {
@@ -54,9 +56,53 @@ export async function getFragmentById(user, id) {
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
-    const data = await res.json();
-    console.log('Got user fragment', { data });
+    const type = res.headers.get('Content-Type');
+    let data; 
+    console.log('Converted to-->' + 'image/png');
+    if(type.startsWith('image/')){ data = await res.text(); }
+    else{ data = await res.json(); }
+    const result = {type: type , data: data};
+    return result;
   } catch (err) {
     console.error('Unable to call GET /v1/fragment/${id}', { err });
+  }
+}
+
+//user can modify a fragment
+export async function modifyFragmentById(user, id, fragment, fragmentType) {
+  console.log('Modifying user fragment data...');
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      method: "put",
+      // Generate headers with the proper Authorization bearer token to pass
+      headers: user.authorizationHeaders(fragmentType),
+      body: fragment,
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Updated user fragment', { data });
+  } catch (err) {
+    console.error('Unable to call PUT /v1/fragment/${id}', { err });
+  }
+}
+
+//user can delete a fragment
+export async function deleteFragmentById(user, id) {
+  console.log('Deleting user fragment data...');
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      method: "delete",
+      // Generate headers with the proper Authorization bearer token to pass
+      headers: user.authorizationHeaders(),
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Deleted fragment', { data });
+  } catch (err) {
+    console.error('Unable to call DELETE /v1/fragment/${id}', { err });
   }
 }
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 //It should use src/auth.js to handle authentication, get the user, and update the UI
 
 import { Auth, getUser } from './auth';
-import { getUserFragments, postUserFragments, getFragmentById} from './api';
+import { getUserFragments, postUserFragments, getFragmentById, deleteFragmentById, modifyFragmentById} from './api';
 
 
 async function init() {
@@ -12,7 +12,11 @@ async function init() {
   const logoutBtn = document.querySelector('#logout');
   const createBtn = document.querySelector('#create');
   const getByIdBtn = document.querySelector('#get');
+  const modifyBtn = document.querySelector('#modify');
+  const deleteBtn = document.querySelector('#delete');
   const selectedType = document.getElementById('fragmentType');
+  const selectedType2 = document.getElementById('fragmentType2');
+  const imagePlace = document.getElementById('imagePlaceholder');
 
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
@@ -40,16 +44,51 @@ async function init() {
   
   createBtn.onclick = async () => {
     console.log('Create fragment button clicked');
-    var input = document.getElementById('fragment').value;
-    console.log(input);
+    var input = document.getElementById('fragment').value; //text
+    var input2 = document.getElementById('fragmentimage'); //file
     console.log(selectedType.value);
-    await postUserFragments(user, JSON.stringify(input), selectedType.value);
+
+    if(selectedType.value.startsWith('image/'))
+    {
+      await postUserFragments(user, input2.files[0], selectedType.value);
+    } 
+    else
+    {
+      await postUserFragments(user, JSON.stringify(input), selectedType.value);
+    }
   };
 
-  getByIdBtn.onclick = () => {
-    var input = document.getElementById('id').value;
+  modifyBtn.onclick = async () => {
+    console.log('Modify fragment button clicked');
+    var input = document.getElementById('modifiedFragment').value; //get fragment data
+    var imageinput = document.getElementById('fragmentimage2'); //get image fragment data
+    var inputID = document.getElementById('modifyID').value; //get id value
+    console.log(selectedType2.value);
+
+    if(selectedType2.value.startsWith('image/')){
+      await modifyFragmentById(user, inputID, imageinput.files[0], selectedType2.value); //image file
+    } else{
+      await modifyFragmentById(user, inputID, JSON.stringify(input), selectedType2.value); //text
+    }
+
+    
+  };
+
+  getByIdBtn.onclick = async () => {
+    var input = document.getElementById('getID').value;
     console.log(input);
-    getFragmentById(user, input);
+    const getFragment  = await getFragmentById(user, input);
+    if(getFragment.type.startsWith('image/')){
+      imagePlace.innerHTML = `<img src="data:${getFragment.type};base64,${getFragment.data}" />`
+    } else{
+      imagePlace.innerHTML = `<pre>${getFragment.data}</pre>`
+    }
+  };
+
+  deleteBtn.onclick = async () => {
+    var input = document.getElementById('deleteID').value;
+    console.log(input);
+    deleteFragmentById(user, input);
   };
 
   // Update the UI to welcome the user
@@ -64,10 +103,6 @@ async function init() {
   // Do an authenticated request to the fragments API server and log the result
   getUserFragments(user);
 }
-
-
-
- 
 
 // Wait for the DOM to be ready, then start the app
 addEventListener('DOMContentLoaded', init);
